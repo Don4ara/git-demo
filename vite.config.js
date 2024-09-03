4da3879ba98bd53b3bd059ceaa83e966e4f42a42
Don4ara <don4araa@gmail.com>
2024-09-03 16:37:57 +0500

    Lox commit

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  base: '/git-demo',
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
